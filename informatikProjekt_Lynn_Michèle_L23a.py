5ba51087ba8211af65e18d86f4adff426a0ffa80
michelewiederkehr <michele@wiederkehr.lu>
2024-05-21 19:44:34 +0200

    Update

--- informatikProjekt_Lynn_Michèle_L23a.py
+++ informatikProjekt_Lynn_Michèle_L23a.py
@@ -23,7 +23,9 @@ background.x = 0
 background1 = Actor("background1.jpg")
 background1.x = WIDTH
 
-introfinished = False
+introfinished1 = False
+introfinished2 = False
+
 startgame = False
 starttext = False
 
@@ -41,24 +43,23 @@ def draw():
         screen.draw.text("Spiel beginnen", left=WIDTH/2 - 180, top=HEIGHT/2, fontsize=60, color=white, fontname="C:\\githubprojects\\game\\fonts\\handlee-regular.ttf", align="center", )
         screen.draw.text("press the spacebar!", left=WIDTH/2 -120, top=HEIGHT/2 + 70, fontsize=30, color=white, fontname="C:\\githubprojects\\game\\fonts\\handlee-regular.ttf", align="center", italic=True, )
     
-    if not introfinished: #prüft ob not introfinished gleich false ist, was in diesem Fall stimmt --> not introfinished wird zu True - und - = + daher wird der Code ausgeführt
+    if not introfinished1: #prüft ob not introfinished1 gleich false ist, was in diesem Fall stimmt --> not introfinished1 wird zu True - und - = + daher wird der Code ausgeführt
         screen.blit("mashrooms", (0, 0)) #fügt hintergrundbild mashrooms ein.
         squirrel.draw()
         screen.blit("mashrooms1", (0, 0)) #mashroom links unten in der Ecke, vor squirrel
         screen.blit("mashrooms2", (0, 0))
         speechbubble.draw()
         black = 0, 0, 0
-        screen.draw.text("Hey!\n> Weiter mit\nTabulatortaste :)", left=385, top=550, fontsize=23, color=black, fontname="C:\\githubprojects\\game\\fonts\\handlee-regular.ttf", align="left") #\n macht einen Brake (Zeilenumbruch) in den text
-    
-    
+        screen.draw.text("Hey!\n> Weiter mit\nTabulatortaste :)", left=385, top=550, fontsize=23, color=black, fontname="..\\fonts\\handlee-regular.ttf", align="left") #\n macht einen Brake (Zeilenumbruch) in den text
+        
 def update():
-    global introfinished, startgame, starttext #damit das False der Variable introfinished, stargame und starttext überschrieben werden darf
+    global introfinished1, startgame, starttext #damit das False der Variable introfinished1, stargame und starttext überschrieben werden darf
     
-    if keyboard.tab and not introfinished: #wenn die tabulatortaste gedrückt wird und das intro noch nicht fertig ist dann soll der Starttext noch nicht angezeigt werden.
-        introfinished = True #prüft andauernd ob space taste gedrückt wurde, und setzt indiesem Falle den wert introfinished bei drücken der Taste auf True, wodurch if not introfinished gleich false ist, denn + und - = -
+    if keyboard.tab and not introfinished1: #wenn die tabulatortaste gedrückt wird und das intro noch nicht fertig ist dann soll der Starttext noch nicht angezeigt werden.
+        introfinished1 = True #prüft andauernd ob space taste gedrückt wurde, und setzt indiesem Falle den wert introfinished1 bei drücken der Taste auf True, wodurch if not introfinished1 gleich false ist, denn + und - = -
         starttext = True
         
-    if keyboard.space and introfinished and not startgame: # wenn die Leertaste gedrückt wird und das intro fertig ist, jedoch das game nch nicht gestartet wurde, soll der Starttext angezeigt werden
+    if keyboard.space and introfinished1 and not startgame: # wenn die Leertaste gedrückt wird und das intro fertig ist, jedoch das game nch nicht gestartet wurde, soll der Starttext angezeigt werden
         startgame = True #das Intro wird somit beendet
         starttext = False #der Starttext wird somit angezeigt
         
